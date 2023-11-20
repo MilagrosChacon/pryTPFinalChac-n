c58697a4d70b0b8072585331aa299a1046383e69
Milagros <milagroschaconies@gmail.com>
2023-11-20 20:52:01 -0300

    código de FormListado y agregado de Base de Datos

--- FormListado.cs
+++ FormListado.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,55 @@ namespace pryTPFinalChacón
 {
     public partial class FormListado : Form
     {
+        private void FormListado_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private ClassEmpleados empleados;
+
         public FormListado()
         {
             InitializeComponent();
+            this.KeyDown += new KeyEventHandler(FormListado_KeyDown);
+
+            empleados = new ClassEmpleados(@"Provider = Microsoft.ACE.OLEDB.12.0;" + " Data Source = ..\\..\\Resources\\EMPLEADO.accdb");
+
+            CargarDatos();
         }
+
+        private void CargarDatos()
+        {
+            DataTable tablaEmpleados = empleados.ObtenerEmpleados();
+            dataGridViewListado.DataSource = empleados;
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            string apellido = txtApellido.Text;
+            string direccion = txtApellido.Text;
+
+            DataTable empleadosFiltrados;
+
+            if (!string.IsNullOrWhiteSpace(apellido))
+            {
+                empleadosFiltrados = empleados.FiltrarPorApellido(apellido);
+            }
+            else if (!string.IsNullOrWhiteSpace(direccion))
+            {
+                empleadosFiltrados = empleados.FiltrarPorDireccion(direccion);
+            }
+            else
+            {
+                CargarDatos();
+                return;
+            }
+
+            dataGridViewListado.DataSource = empleadosFiltrados;
+        }
+    }
     }
 }
